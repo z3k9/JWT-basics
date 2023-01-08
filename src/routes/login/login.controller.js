a006fe804b105e928d91176b058ff1bd4aea8766
z3k9 <ajelelade@gmail.com>
2023-01-08 16:36:36 +0100

    added mock auth to the controller

--- src/routes/login/login.controller.js
+++ src/routes/login/login.controller.js
@@ -6,22 +6,36 @@
 const jwt = require('jsonwebtoken');
 
 async function login(req, res){
-    const token = jwt.sign()
+    const id = new Date().getDate();
     const {username, password }= req.body;
+    // try to keep payload small, better experience for user
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'});
+    
     // mongo
     // Joi
     // check in the controller 
 
     if(!username || !password){
-        throw new CustomAPIError('Please provide email and password', 400)
+        return res.status(400).json({msg: 'Please provide a correct username and password'})
     }
 
-    return res.status(200).send('Fake Login/Register/Signup Route');
+    return res.status(200).json({msg: 'user created', token});
 }
 
 async function dashboard(req,res){
-    const luckyNumber = Math.floor(Math.random()*100);
-    return res.status(200).json({msg: `Hello, John Doe`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`});
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(401).json({msg: 'token not provided'})
+    }
+    const token = authHeader.split(' ')[1];
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const luckyNumber = Math.floor(Math.random()*100);
+        return res.status(200).json({msg: `Hello, ${decoded.username}`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`});
+    } catch(error){
+        return res.status(400).json({msg: 'Not authorized to access this route'});
+    }
+    
 }
 
 module.exports = { login, dashboard };
